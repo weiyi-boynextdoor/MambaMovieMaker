9dc8aa5d8b084b8ebb9f1c54101c960f944281c5
weiyi <weiyiboynextdoor@163.com>
2026-08-15 18:38:08 +0800

    support cookies

--- download_video.py
+++ download_video.py
@@ -6,7 +6,7 @@ def download(
     url: str,
     # output_template: str | None = None,
     # cookies_from_browser: str | None = None,
-    # cookies_file: str | None = None,
+    cookies_file: str | None = None,
 ) -> None:
     """Download a video from the specified URL.
 
@@ -18,18 +18,18 @@ def download(
         A yt-dlp template for naming the output file. If omitted the default
         template from yt-dlp will be used (e.g. "%(title)s.%(ext)s").
     """
-    # ydl_opts: dict = {
-    #     "http_headers": {
-    #         "Origin": "https://www.bilibili.com",
-    #         "Referer": "https://www.bilibili.com/",
-    #     },
-    # }
+    ydl_opts: dict = {
+        "http_headers": {
+            # "Origin": "https://www.bilibili.com",
+            # "Referer": "https://www.bilibili.com/",
+        },
+    }
     # if output_template:
     #     ydl_opts["outtmpl"] = output_template
     # if cookies_from_browser:
     #     ydl_opts["cookiesfrombrowser"] = (cookies_from_browser,)
-    # if cookies_file:
-    #     ydl_opts["cookiefile"] = cookies_file
+    if cookies_file:
+        ydl_opts["cookiefile"] = cookies_file
 
     with YoutubeDL() as ydl:
         ydl.download([url])
@@ -45,18 +45,18 @@ def main() -> None:
     #     "--cookies-from-browser",
     #     help='Read cookies from a browser, for example "chrome" or "edge".',
     # )
-    # parser.add_argument(
-    #     "--cookies",
-    #     dest="cookies_file",
-    #     help="Read cookies from a Netscape-format cookies.txt file.",
-    # )
+    parser.add_argument(
+        "--cookies",
+        dest="cookies_file",
+        help="Read cookies from a Netscape-format cookies.txt file.",
+    )
     args = parser.parse_args()
 
     download(
         args.url,
         # args.output_template,
         # cookies_from_browser=args.cookies_from_browser,
-        # cookies_file=args.cookies_file,
+        cookies_file=args.cookies_file,
     )
 
 
